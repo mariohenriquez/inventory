6944a918ab78cf0b8155eadeecf4da82760cfbe2
Mario Henriquez <mariohenriquez@Marios-MBP.localdomain>
2023-08-02 17:34:46 -0500

    changing header path

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,6 @@
-import Header from "../components/header";
-
 export default function Layout({ children }) {
     return (
         <>
-            <Header></Header>
             <div className='max-w-screen-xl mx-auto pt-[72px]'>{children}</div>
         </>
     );
--- pages/invoice/index.js
+++ pages/invoice/index.js
@@ -1,8 +1,10 @@
 import Layout from '../../components/layout';
+import Header from '../../components/header';
 
 export default function Invoice() {
     return (
         <Layout>
+            <Header />
             <div className='py-5'>
                 <h1 className='text-3xl'>Invoice</h1>
                 <div>
