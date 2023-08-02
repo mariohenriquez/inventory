a82e8b00ffdd6af9aaa5f0e654e3e41c5f64acf8
Mario Henriquez <mariohenriquez@Marios-MBP.localdomain>
2023-08-02 17:40:22 -0500

    changing header path

--- pages/inventory/index.js
+++ pages/inventory/index.js
@@ -1,12 +1,10 @@
 import Layout from '../../components/layout';
-import Header from '../../components/header';
 import Grid from '../../components/Grid';
 import homes from 'data.json';
 
 export default function Inventory() {
     return (
         <Layout>
-            <Header />
             <div className='py-5'>
                 <h1 className='text-3xl mb-5'>Inventory</h1>
                 <div className=''>
