704cc706ddf9c8c8e02bdc29da61dd81519d1b4e
Mario Henriquez <mariohenriquez@Marios-MBP.localdomain>
2023-08-02 17:44:27 -0500

    changing header path - 4

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Header from "../components/header";
+
 
 export default function Home() {
   return (
     <>
-      <Header />
       <div className="max-w-screen-xl mx-auto pt-[72px]">
         <div className="py-5">
           <h1 className="text-3xl">Main Content</h1>
